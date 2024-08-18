23d57c7960a8b62faec78e88d6e99439eaa346e0
mahmutcant <mahcan012@gmail.com>
2024-08-18 22:37:12 +0300

    checkbox filter definition

--- src/app/[lang]/mainpage/important-words/WordContainer.tsx
+++ src/app/[lang]/mainpage/important-words/WordContainer.tsx
@@ -1,51 +1,146 @@
 "use client"
-import { auth, db } from '@/firebase';
-import { onAuthStateChanged } from 'firebase/auth';
-import { onValue, ref } from 'firebase/database';
 import React, { useEffect, useState } from 'react'
 import WordContext from './WordContext';
+import data from "../../../../../wordlist.json";
 type WordListObject = {
     [key: number]: { [word: string]: string };
 };
+
 const WordContainer = () => {
     const [wordList, setWordList] = useState<WordListObject>({})
+    const [savedIndexes, setSavedIndexes] = useState<string[]>([]);
+    const [showScrollToTop, setShowScrollToTop] = useState<boolean>(false);
+    const [showKnonwWords, setKnownWords] = useState<boolean>(false);
+    const [showUnknownWords, setShowUnknownWords] = useState<boolean>(false);
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                const educationContextRef = ref(db, "Exams/YDS/word/");
-                onValue(educationContextRef, (snapshot) => {
-
-                    const data = snapshot.val() as WordListObject | null;
-
-                    if (data) {
-                        const words = Object.values(data).map(obj => {
-                            const key = Object.keys(obj)[0];
-                            return { [key]: obj[key] };
-                        });
-
-                        console.log(words);
-
-                        setWordList(words);
-                    } else {
-                        setWordList({});
-                    }
-                });
-            }
-        });
-        return () => unsubscribe();
+        const saved = JSON.parse(localStorage.getItem("words") || "[]");
+        setSavedIndexes(saved);
+        setWordList(data)
     }, []);
 
-    return (
-        <>
-            {wordList && Object.values(wordList).map((wordObj, index) => {
-                const key = Object.keys(wordObj)[0];
-                const value = wordObj[key];
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setShowScrollToTop(true);
+            } else {
+                setShowScrollToTop(false);
+            }
+        };
 
-                return (
-                    <WordContext key={key + " " + index} item={key} value={value} />
-                );
-            })}
-        </>
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    useEffect(() => {
+        console.log(showScrollToTop);
+
+    }, [showScrollToTop])
+    const saveKnownWord = (index: string, value: boolean) => {
+        if (value) {
+            if (!savedIndexes.includes(index)) {
+                const updatedIndexes = [...savedIndexes, index];
+                setSavedIndexes(updatedIndexes);
+                localStorage.setItem("words", JSON.stringify(updatedIndexes));
+            }
+        } else {
+            const updatedIndexes = savedIndexes.filter(i => i !== index);
+            setSavedIndexes(updatedIndexes);
+            localStorage.setItem("words", JSON.stringify(updatedIndexes));
+        }
+    }
+    const WordContainerReturner = () => {
+        if (showKnonwWords) {
+            return (
+                <>
+                    {Object.keys(wordList)
+                        .map(key => parseInt(key))
+                        .filter(key => savedIndexes.includes(key.toString()))
+                        .map((key, index) => {
+                            const wordObj = wordList[key];
+                            const word = Object.keys(wordObj)[0];
+                            const value = wordObj[word];
+                            return (
+                                <WordContext
+                                    key={word + " " + index}
+                                    item={word}
+                                    value={value}
+                                    index={key.toString()}
+                                    saveKnownWord={saveKnownWord}
+                                    isChecked={savedIndexes.includes(key.toString())}
+                                />
+                            );
+                        })}
+                </>
+            );
+        } else if (showUnknownWords) {
+            return (
+                <>
+                    {Object.keys(wordList)
+                        .map(key => parseInt(key))
+                        .filter(key => !savedIndexes.includes(key.toString()))
+                        .map((key, index) => {
+                            const wordObj = wordList[key];
+                            const word = Object.keys(wordObj)[0];
+                            const value = wordObj[word];
+                            return (
+                                <WordContext
+                                    key={word + " " + index}
+                                    item={word}
+                                    value={value}
+                                    index={key.toString()}
+                                    saveKnownWord={saveKnownWord}
+                                    isChecked={savedIndexes.includes(key.toString())}
+                                />
+                            );
+                        })}
+                </>
+            );
+        } else {
+            return (
+                <>
+                    {Object.values(wordList).map((wordObj, index) => {
+                        const key = Object.keys(wordObj)[0];
+                        const value = wordObj[key];
+                        return (
+                            <WordContext
+                                key={key + " " + index}
+                                item={key}
+                                value={value}
+                                index={Object.keys(wordList)[index]}
+                                saveKnownWord={saveKnownWord}
+                                isChecked={savedIndexes.includes(Object.keys(wordList)[index])}
+                            />
+                        );
+                    })}
+                </>
+            );
+        }
+    };
+    return (
+        <div>
+            <div className={`w-full bg-white mx-3 ${showScrollToTop && "fixed top-0"} flex items-center p-3`}>
+                <input className='border rounded-full p-2' type="text" placeholder='Search' />
+                <div className="flex flex-col mx-3">
+                    <input id="default-checkbox" type="checkbox" value=""
+                        onChange={(e) => setShowUnknownWords(e.currentTarget.checked)}
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-600 text-[12px]">
+                        Show Unknown Words
+                    </label>
+                    <input id="default-checkbox" type="checkbox"
+                        onChange={(e) => setKnownWords(e.currentTarget.checked)}
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-600 text-[12px]">
+                        Show Known Words
+                    </label>
+                </div>
+            </div>
+            <div className='mt-3'>
+                {WordContainerReturner()}
+            </div>
+        </div>
     )
 }
 
--- src/app/[lang]/mainpage/important-words/WordContext.tsx
+++ src/app/[lang]/mainpage/important-words/WordContext.tsx
@@ -1,27 +1,39 @@
 import React, { useState } from 'react'
 export type WordContextModel = {
     item: string,
-    value: string
+    value: string,
+    index: string,
+    saveKnownWord: (index:string,value:boolean) => void,
+    isChecked: boolean
 }
-const WordContext:React.FC<WordContextModel> = ({item, value}) => {
+const WordContext: React.FC<WordContextModel> = ({ item, value, index, saveKnownWord,isChecked}) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
-  return (
-    <div className="border-b border-gray-200">
+    return (
+        <div className="border-b border-gray-200">
             <button
                 onClick={() => setIsOpen(!isOpen)}
                 className="w-full text-left p-4 font-semibold text-gray-800 flex justify-between items-center"
             >
+                
                 <span>{item}</span>
-                <span>{isOpen ? '-' : '+'}</span>
+                <div className="">
+                    <input
+                        id="link-checkbox"
+                        type="checkbox"
+                        checked={isChecked}
+                        className="w-4 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
+                dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 mr-3"
+                        onClick={(e) => {e.stopPropagation(); saveKnownWord(index,e.currentTarget.checked)}}
+                    />
+                </div>
             </button>
             <div
-                className={`transition-max-height duration-700 ease-in-out overflow-hidden ${isOpen ? 'max-h-screen' : 'max-h-0'
-                    }`}
+                className={`transition-max-height duration-700 ease-in-out overflow-hidden ${isOpen ? 'max-h-screen' : 'max-h-0'}`}
             >
-                <div className="p-4 text-gray-600">{value}</div>
+                <div className="p-4 text-gray-600 text-center">{value}</div>
             </div>
         </div>
-  )
+    )
 }
 
 export default WordContext
