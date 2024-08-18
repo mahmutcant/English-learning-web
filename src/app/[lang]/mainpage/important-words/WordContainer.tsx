3927b83c0ff2224aa33403c50194393db759030f
mahmutcant <mahcan012@gmail.com>
2024-08-18 16:36:21 +0300

    db update

--- src/app/[lang]/mainpage/important-words/WordContainer.tsx
+++ src/app/[lang]/mainpage/important-words/WordContainer.tsx
@@ -4,36 +4,47 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { onValue, ref } from 'firebase/database';
 import React, { useEffect, useState } from 'react'
 import WordContext from './WordContext';
-type WordListObjest = {
-    [key: string] : string;
-}
+type WordListObject = {
+    [key: number]: { [word: string]: string };
+};
 const WordContainer = () => {
-    const [wordList,setWordList] = useState<WordListObjest>({})
+    const [wordList, setWordList] = useState<WordListObject>({})
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
                 const educationContextRef = ref(db, "Exams/YDS/word/");
                 onValue(educationContextRef, (snapshot) => {
-                    
-                    const data = snapshot.val();
-                    console.log(data);
+
+                    const data = snapshot.val() as WordListObject | null;
+
                     if (data) {
-                        const words = Object.keys(data);
-                        setWordList(data)
+                        const words = Object.values(data).map(obj => {
+                            const key = Object.keys(obj)[0];
+                            return { [key]: obj[key] };
+                        });
+
+                        console.log(words);
+
+                        setWordList(words);
                     } else {
-                        setWordList({})
+                        setWordList({});
                     }
                 });
             }
         });
         return () => unsubscribe();
     }, []);
-    
+
     return (
         <>
-        {wordList && Object.keys(wordList).map((key,index) => (
-            <WordContext key={wordList[key] + " " + index} item={key} value={wordList[key]} />
-        ))}
+            {wordList && Object.values(wordList).map((wordObj, index) => {
+                const key = Object.keys(wordObj)[0];
+                const value = wordObj[key];
+
+                return (
+                    <WordContext key={key + " " + index} item={key} value={value} />
+                );
+            })}
         </>
     )
 }
